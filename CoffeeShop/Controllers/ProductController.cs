f528f9f142e2e0bdde5957a9bd1c241507103d6b
Ryan Coughran <ryancoughran@quickenloans.com>
2021-09-14 15:24:30 -0700

    initial commit

---
+++ CoffeeShop/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using Dapper.Contrib.Extensions;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            MySqlConnection db = new MySqlConnection("Server=localhost;Database=coffeeshop;Uid=root;Password=abc123");
+            List<Product> prods = db.GetAll<Product>().ToList();
+           
+            Console.WriteLine();
+            return View(prods);
+        }
+
+        public IActionResult Detail(int prodId)
+        {
+            MySqlConnection db = new MySqlConnection("Server=localhost;Database=coffeeshop;Uid=root;Password=abc123");
+            Product thisProd = db.Get<Product>(prodId);
+            return View(thisProd);
+        }
+    }
+}
---
+++ CoffeeShop/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+
+namespace CoffeeShop.Models
+{
+    [Table("products")]
+    public class Product
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public string category { get; set; }
+       
+    }
+}
